bc778b1bb190a1245a1661aaea17ac81f8880247
Thomas Verschoren <thomas@verschoren.com>
2020-07-01 14:18:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,16 @@
 
 echo "Deploying to Zendesk..."
 FILE=.zat
+
 PASSWORD="\"password\""
 APP_ID="\"app_id\""
 
 if  grep -q "$APP_ID" "$FILE" ; then
-    echo 'App ID detected, running update' ; 
+    echo "App ID detected, running update" ; 
     zat update
 elif grep -q "$PASSWORD" "$FILE" ; then
-    echo 'No ID detected, creating new app.' ; 
+    echo "No ID detected, creating new app." ; 
     zat create
 else
-    echo 'can\'t run the script' ; 
+    echo "can't run the script" ; 
 fi
