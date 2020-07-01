2e62b726a8e94885dd9fe854671f6e623ee55db9
Thomas Verschoren <thomas@verschoren.com>
2020-07-01 13:33:10 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
 echo "Deploying to Zendesk..."
+FILE=/.zat
 
-zat update
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+    zat update
+else 
+    echo "$FILE does not exist."
+fi
