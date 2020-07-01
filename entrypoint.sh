e2aa027639bbb12a550ad2739d46ed3605c10c0c
Thomas Verschoren <thomas@verschoren.com>
2020-07-01 14:05:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,9 @@ FILE=.zat
 
 STRING="\app_id\""
 
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-    if  grep -q "$STRING" "$FILE" ; then
-         echo 'the string exists' ; 
-         zat update
-    else
-         echo 'the string does not exist' ; 
-    fi
-else 
-    echo "$FILE does not exist."
+if  grep -q "$STRING" "$FILE" ; then
+    echo 'the string exists' ; 
+    zat update
+else
+    echo 'the string does not exist' ; 
 fi
