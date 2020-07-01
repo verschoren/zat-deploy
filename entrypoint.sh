18a1efe10c242a7361d152f0aa77e600fe7f0b36
Thomas Verschoren <thomas@verschoren.com>
2020-07-01 14:21:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ if  grep -q "$APP_ID" "$FILE" ; then
     echo "App ID detected, running update" ; 
     zat update
 elif grep -q "$PASSWORD" "$FILE" ; then
-    echo "No ID detected, creating new app." ; 
+    echo "No existing APP ID detected, creating new app." ; 
     zat create
 else
-    echo "can't run the script" ; 
+    echo "Can't deploy. Please check your .zat file" ; 
 fi
