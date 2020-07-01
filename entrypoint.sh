78fdbb2d53db2fc510b451616e766f17f03cf4f4
Thomas Verschoren <thomas@verschoren.com>
2020-07-01 13:37:48 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ FILE=.zat
 
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
+    if ! grep -q password "$File"; then
+        Some Actions # SomeString was not found
+    fi
     zat update
 else 
     echo "$FILE does not exist."
